ea4c6820e99c6b811041f006420c030db608b52b
saad <saadgn89@gmail.com>
2025-10-03 23:32:10 +0500

    Db setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: '#pakistan123',
+        database: 'project-nestjs',
+        synchronize: true,
+        autoLoadEntities: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
