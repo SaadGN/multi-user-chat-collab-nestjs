bee8cc31c78949470b47e1d247772b5997e30230
saad <saadgn89@gmail.com>
2025-10-03 23:30:01 +0500

    configured database with user entity

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    constructor(
+        @InjectRepository(User)
+        private userRepositry: Repository<User>
+
+    ) { }
+
+
+    public async getUsers() {
+        return "List of all users"
+    }
+}
