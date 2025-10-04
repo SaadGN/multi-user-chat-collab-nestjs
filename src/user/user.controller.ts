ea19fc664d313ee51f58aa1e0d7d4b8f55c3d7d3
saad <saadgn89@gmail.com>
2025-10-05 01:47:00 +0500

    create ,delet,getAll  user features added

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get ,Param,ParseIntPipe,Post ,} from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Controller('user')
 export class UserController {
@@ -10,4 +11,15 @@ export class UserController {
     getUsers(){
         return this.userService.getUsers()
     }
+
+    @Post('')
+    createUser(@Body() userDto:CreateUserDto){
+        return this.userService.createUser(userDto)
+    }
+
+    @Delete(':id')
+    deleteUser(@Param('id',ParseIntPipe) id:number){
+        return this.userService.deleteUser(id)
+    }
+
 }
