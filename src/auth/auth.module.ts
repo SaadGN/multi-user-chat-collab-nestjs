747c4257cf6696936660aed4d01213d7663d677a
saad <saadgn89@gmail.com>
2025-10-08 03:08:21 +0500

    jwt config module setup

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AdminController } from 'src/admin/admin.controller';
-import { AdminService } from 'src/admin/admin.service';
+import { UserModule } from 'src/user/user.module';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import authConfig from '../config/auth.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    controllers:[AdminController],
-    providers:[AdminService],
+    controllers: [AuthController],
+    providers: [AuthService],
+    imports: [
+        UserModule,
+        JwtModule.registerAsync(authConfig.asProvider()),
+        ConfigModule.forFeature(authConfig)
+    ],
+    exports: [AuthService]
 })
-export class AuthModule {}
+export class AuthModule { }
