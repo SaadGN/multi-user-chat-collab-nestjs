cb51c9dee62cd9d7ec225081a68141e772792cd3
SaadGN <saadgn89@gmail.com>
2025-10-10 20:33:03 +0500

    hide password function

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,24 +1,25 @@
-import { Body, Controller, Delete, Get ,Param,ParseIntPipe,Post ,} from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Delete, Get, Param, ParseIntPipe, Post, UseInterceptors } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 
+@UseInterceptors(ClassSerializerInterceptor)
 @Controller('user')
 export class UserController {
 
-    constructor(private readonly userService:UserService){}
+    constructor(private readonly userService: UserService) { }
 
     @Get('')
-    getUsers(){
+    getUsers() {
         return this.userService.getUsers()
     }
 
     @Post('')
-    createUser(@Body() userDto:CreateUserDto){
+    createUser(@Body() userDto: CreateUserDto) {
         return this.userService.createUser(userDto)
     }
 
     @Delete(':id')
-    deleteUser(@Param('id',ParseIntPipe) id:number){
+    deleteUser(@Param('id', ParseIntPipe) id: number) {
         return this.userService.deleteUser(id)
     }
 
