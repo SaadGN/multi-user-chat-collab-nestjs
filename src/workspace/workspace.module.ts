b509010ee135fc8fd2fdbb5d7d1646560795a437
SaadGN <saadgn89@gmail.com>
2025-10-11 04:07:55 +0500

    added jwt module

--- src/workspace/workspace.module.ts
+++ src/workspace/workspace.module.ts
@@ -3,12 +3,16 @@ import { WorkspaceController } from './workspace.controller';
 import { WorkspaceService } from './workspace.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Workspace } from './entity/workspace.entity';
+import { JwtModule } from '@nestjs/jwt';
+import authConfig from 'src/config/auth.config';
 
 @Module({
   controllers: [WorkspaceController],
   providers: [WorkspaceService],
   imports: [
-    TypeOrmModule.forFeature([Workspace])
+    TypeOrmModule.forFeature([Workspace]),
+    JwtModule.registerAsync(authConfig.asProvider())
+
   ]
 })
 export class WorkspaceModule { }
