b3a10ca54943baf8df1041926cb630537c20cc05
SaadGN <saadgn89@gmail.com>
2025-10-12 04:44:32 +0500

    added invite endpoint

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Body, Post } from '@nestjs/common';
+import { CreateInviteDto } from './invite/dto/invite.dto';
+import { AdminService } from './admin.service';
 
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+    constructor(
+        private readonly adminService:AdminService
+    ) { }
+
+    @Post('invite')
+    sendInviteLink(
+        @Body() inviteDto:CreateInviteDto
+    ){
+        return this.adminService.sendInviteLink(inviteDto)
+    }
+
+}
