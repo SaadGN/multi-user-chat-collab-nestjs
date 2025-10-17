1b7f7ece7598117f42f990d3cf0206ade45e5ac9
SaadGN <saadgn89@gmail.com>
2025-10-18 01:47:59 +0500

    added signup controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Query } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dtos/login.dto';
+import { CreateUserDto } from 'src/user/dtos/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -12,5 +13,13 @@ export class AuthController {
     @Post('login')
     async loginUser(@Body() loginDto: LoginDto) {
         return await this.authService.loginUser(loginDto)
+    }
+
+    @Post('signup')
+    async signup(
+        @Query('token') token: string,
+        @Body() userDto: CreateUserDto
+    ) {
+        return await this.authService.signup(token,userDto )
     }
 }
