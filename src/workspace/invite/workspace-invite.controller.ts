e2066b15641556ca115fc2d08763b4aa97024fa8
SaadGN <saadgn89@gmail.com>
2026-03-01 03:07:46 +0500

    workspace controller created

---
+++ src/workspace/invite/workspace-invite.controller.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Get, Post, Query, UseGuards } from "@nestjs/common";
+import { WorkspaceInviteService } from "./workspace-invite.service";
+import { CreateWorkspaceInviteDto } from "./dto/create-workspace-invite.dto";
+import { AuthorizeGuard } from "src/guards/authorize.guard";
+import { AdminDecorator } from "src/auth/decorators/admin.decorator";
+
+
+@Controller('workspace/invite')
+export class WorkspaceInviteController{
+    constructor(
+        private readonly workspaceInviteService:WorkspaceInviteService
+    ){}
+
+    @UseGuards(AuthorizeGuard)
+    @AdminDecorator()
+    @Post()
+    async sendInvite(@Body() inviteDto:CreateWorkspaceInviteDto){
+        return await this.workspaceInviteService.sendWorkspaceInvite(inviteDto);
+    }
+
+    @UseGuards(AuthorizeGuard)
+    @Get()
+    async acceptInvite(@Query('token') token:string){
+        const result = await this.workspaceInviteService.acceptWorkspaceInvite(token);
+        return result;
+    }
+}
